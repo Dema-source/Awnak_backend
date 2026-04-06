17559c4e69e3d7b705fe51c04442c6dd4e0c2dfe
ZakariaZhlat125 <zakooorr1567@gmail.com>
2026-04-06 21:27:33 +0300

    solve error lang

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -20,7 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
-            'translate' => \App\Http\Middleware\SetLocaleFromHeader::class,
+            // 'translate' => \App\Http\Middleware\SetLocaleFromHeader::class,
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_has_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
